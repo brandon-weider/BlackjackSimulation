2502e6bb55a91e10926ca8c50cbea81eca3419b4
brandon-weider <bgw123@psu.edu>
2013-09-22 20:15:57 -0400

    redid the code introduced while loops

--- Weider Blackjack Simulator.cpp
+++ Weider Blackjack Simulator.cpp
@@ -19,38 +19,13 @@ char playAgain;
 char hitStand;
 char win; //might not use
 void failurePA ();
-void successHS ();
-void failureHS ();
 void successPA ();
 int main ();
 
 //functions used to determine if they used correct letters during the command prompt
-void failureHS () {
-	cout << "Please enter either an H or a S." << endl;
-	cout << "Would you like to hit or stay (H/S)" << endl;
-	cin >> hitStand;
-	if (hitStand != 'H' && hitStand != 'h' && hitStand != 'S' && hitStand !='s') {
-		failureHS ();
-	}
-	else if (hitStand == 'H' || hitStand == 'h' || hitStand == 'S' || hitStand =='s') {
-		successHS (); 
-	}
-}
-
-void successHS () {
-	if (hitStand == 'H' || hitStand == 'h'){
-		playerCardHit = 2 + rand() % 9;
-		playerTotal += playerCardHit;
-		cout << "Your new card total is " << playerTotal << endl;
-	}
-	if (hitStand == 'S' || hitStand == 's'){
-		cout << "Your total will stay at " << playerTotal << endl;
-	}
-}
-
 void failurePA () {
 	cout << "Please enter either a Y or a N." << endl;
-	cout << "Would you like to play another hand? (Y/N)";
+	cout << "Would you like to play another hand? (Y/N) ";
 	cin >> playAgain;
 	if (playAgain != 'Y' && playAgain != 'y' && playAgain != 'N' && playAgain != 'n') {
 		failurePA ();
@@ -72,17 +47,17 @@ void successPA () {
 
 int main () {
 	if (newBank <= 0) {
-		cout << "Sorry you have no more money left to bet. Thank you for playing." << endl;
+		cout << "Sorry you have no more money left to bet. Thank you for playing.";
 		return 0;
 	}
 	
 	cout << "You have a total of " << newBank << " dollars to bet with." << endl;
-	cout << "How much would you like to bet? (You cannot bet more than you have)" << endl;
+	cout << "How much would you like to bet? (You cannot bet more than you have) ";
 	cin >> playerBet;
 
 	if (playerBet > newBank) {
 		cout << "Sorry you do not have that much money to bet with. Please make a smaller bet." << endl;
-		cout << "How much would you like to bet? (You cannot bet more than you have)" << endl;
+		cout << "How much would you like to bet? (You cannot bet more than you have) ";
 		cin >> playerBet;
 	}
 
@@ -95,20 +70,27 @@ int main () {
 
 	cout << "Your card values are " << playerCardOne << " and " << playerCardTwo << endl;
 	cout << "The dealer\'s first card is " << dealerCardOne << endl;
-	cout << "Your card values total up to " << playerTotal << ". Would you like to hit or stand? (H/S)" << endl;
-	cin >> hitStand;
+	cout << "Your card values total up to " << playerTotal << "." << endl;
+	hitStand = 'h';
 
-	if (hitStand != 'H' && hitStand != 'h' && hitStand != 'S' && hitStand != 's') {
-		failureHS ();
+	while ((playerTotal < 21) && (hitStand == 'H' || hitStand == 'h')) {
+		cout << "Would you like to hit or stand? (H/S) ";
+		cin >> hitStand;
+		if (hitStand == 'H' || hitStand == 'h') {
+			playerCardHit = 2 + rand() % 9;
+			playerTotal += playerCardHit;
+			cout << "You drew a " << playerCardHit << ". Your new card total is " << playerTotal << endl;
+		}
 	}
-	else if (hitStand == 'H' || hitStand == 'h' || hitStand == 'S' || hitStand == 's') {
-		successHS ();
+	
+	if (hitStand == 'S' || hitStand == 's') {
+			cout << "Your total will stay at " << playerTotal << "." << endl;
 	}
 
 	if (playerTotal > 21) {
 		newBank -= playerBet;
 		cout << "Sorry you bust. You lost the game." << endl;
-		cout << "Would you like to play another hand? (Y/N)" << endl;
+		cout << "Would you like to play another hand? (Y/N) ";
 		cin >> playAgain;
 		if (playAgain != 'Y' && playAgain != 'y' && playAgain != 'N' && playAgain != 'n') {
 			failurePA ();
@@ -117,18 +99,36 @@ int main () {
 			successPA ();
 		}
 	}
+	else if (playerTotal == 21) {
+		cout << "Good job! You got blackjack! Let\'s see what the dealer ends up with" << endl;
+	}
 	
 	cout << "The dealer\'s second card is " << dealerCardTwo << endl;
 	cout << "The dealer has a total of " << dealerTotal << endl;
-	
-	if (dealerTotal < 17) {
+
+	while (dealerTotal < 17) {
 		dealerDraw = 2 + rand() % 9;
-		cout << "The dealer drew a " << dealerDraw << endl;
 		dealerTotal += dealerDraw;
-		if (dealerTotal > 21) {
-			newBank += (playerBet * 2);
-			cout << "The dealer has busted. You win!" << endl;
-			cout << "Would you like to play another hand? (Y/N)" << endl;
+		cout << "The dealer drew a " << dealerDraw << ". His new total is " << dealerTotal << "." << endl;
+	}
+
+	if (dealerTotal > 21) {
+		newBank += (playerBet * 2);
+		cout << "The dealer has busted. You win!" << endl;
+		cout << "Would you like to play another hand? (Y/N) ";
+		cin >> playAgain;
+		if (playAgain != 'Y' && playAgain != 'y' && playAgain != 'N' && playAgain != 'n') {
+			failurePA ();
+		}
+		else if (playAgain == 'Y' || playAgain == 'y' || playAgain == 'N' || playAgain == 'n') {
+			successPA ();
+		}
+	}
+	else if (dealerTotal == 21) {
+		cout << "The dealer got blackjack!" << endl;
+		if (playerTotal < dealerTotal) {
+			newBank -= playerBet;
+			cout << "You lost the game. Would you like to play another hand? (Y/N) ";
 			cin >> playAgain;
 			if (playAgain != 'Y' && playAgain != 'y' && playAgain != 'N' && playAgain != 'n') {
 				failurePA ();
@@ -138,9 +138,21 @@ int main () {
 			}
 		}
 	}
-	else if (playerTotal > dealerTotal) {
+	else if (dealerTotal == playerTotal) {
+		newBank = playerBet;
+		cout << "You have tied the dealer! Would you like to play another hand? (Y/N) ";
+		cin >> playAgain;
+		if (playAgain != 'Y' && playAgain != 'y' && playAgain != 'N' && playAgain != 'n') {
+			failurePA ();
+		}
+		else if (playAgain == 'Y' || playAgain == 'y' || playAgain == 'N' || playAgain == 'n') {
+			successPA ();
+		}
+	}
+
+	if (playerTotal > dealerTotal) {
 		newBank += (playerBet * 2);
-		cout << "You win! Would you like to play another hand? (Y/N)" << endl;
+		cout << "You win! Would you like to play another hand? (Y/N) ";
 		cin >> playAgain;
 		if (playAgain != 'Y' && playAgain != 'y' && playAgain != 'N' && playAgain != 'n') {
 			failurePA ();
@@ -151,7 +163,7 @@ int main () {
 	}
 	else if (playerTotal < dealerTotal) {
 		newBank -= playerBet;
-		cout << "You lost the game. Would you like to play another hand? (Y/N)" << endl;
+		cout << "You lost the game. Would you like to play another hand? (Y/N) ";
 		cin >> playAgain;
 		if (playAgain != 'Y' && playAgain != 'y' && playAgain != 'N' && playAgain != 'n') {
 			failurePA ();
@@ -160,5 +172,4 @@ int main () {
 			successPA ();
 		}
 	}
-	
 }
